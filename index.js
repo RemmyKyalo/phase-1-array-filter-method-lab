bb7b5d68edafcfd983624e9c0b96cdd2b754aced
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-29 13:04:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+
+function findMatching(driver, name) {
+    return driver.filter(
+        (matching) => matching.toLowerCase() === name.toLowerCase());
+    }
+function fuzzyMatch(driver,name){
+
+    return driver.filter(
+        (matching)=>matching.charAt(0)===name.charAt(0));
+    
+}
+
+function matchName(driver, match) {
+    return driver.filter((drivers) => drivers.name === match);
+
+}
